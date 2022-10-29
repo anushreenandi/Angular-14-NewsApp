003e1e0a3a82a5ff7bc02ba2fb092ed4dc7ceebf
anushreenandi <110711445+anushreenandi@users.noreply.github.com>
2022-10-29 11:15:05 +0530

    first commit

---
+++ src/app/service/news.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NewsService {
+
+  api_key = '75700185ba2e418eb9264ca2ae894f50';
+  constructor(private http: HttpClient) { }
+
+
+  initSources() {
+    return this.http.get('https://newsapi.org/v2/sources?language=en&apiKey=' + this.api_key);
+  }
+
+
+  initArticles() {
+    return this.http.get('https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=' + this.api_key);
+  }
+
+  getArticlesByID(source: String) {
+    return this.http.get('https://newsapi.org/v2/top-headlines?sources=' + source + '&apiKey=' + this.api_key);
+  }
+
+}
